92eca553d0bbe15a6882e111ae00954a1c79e634
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-07-09 23:44:07 +0700

    add: meanmbahkan folder database/connect.php

--- database/connect.php
+++ database/connect.php
@@ -5,7 +5,7 @@ $dbname = "pesantren";
 $password = "";
 
 try {
-    $dbc = new PDO(`mysql:host={$host};dbname={$dbname}`, $username, $password);
+    $dbc = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
     $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $err) {
     echo $err->getMessage();
