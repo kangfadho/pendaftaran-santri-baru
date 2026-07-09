477ea05dfc96ee25aefda4d3fa5668b8be406a20
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-07-10 00:17:43 +0700

    add: meanmbahkan folder database/connect.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+require_once "./database/function.php";
+
 
 ?>
+
+<form action="#" method="POST">
+    <label for="username">Masukkan username :</label>
+    <input type="username" name="username">
+</form>
